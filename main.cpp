4cd1816a019f8fd9ef960625bde81d05725c1941
Hamish McDonald <141893103+hamishmcd123@users.noreply.github.com>
2025-02-12 14:59:55 +1100

    added main file for testing

--- main.cpp
+++ main.cpp
@@ -5,9 +5,33 @@
 
 
 int main() {
-	String test1 = "abc";
-	std::cout << test1[5];
-	std::cout << test1;
-
-	std::this_thread::sleep_for(std::chrono::seconds(2));
+	String teststring = "This is a test string using the string utility";
+	std::cout << '\n';
+	std::cout << "Original string: " << teststring << '\n';
+	std::cout << '\n';
+	std::cout << teststring.ToUpper() << '\n';
+	std::cout << '\n';
+	std::cout << teststring.ToLower() << '\n';
+	std::cout << '\n';
+	teststring.WriteToConsole(); 
+	std::cout << '\n';
+	std::cout << "Relacing 'i' with 'a' -- replacements : " << teststring.Replace('i', 'a') << '\n' << teststring;
+	std::cout << '\n';
+	String teststring2 = "This is a string to append"; 
+	std::cout << '\n';
+	std::cout << teststring2 << '\n';
+	std::cout << '\n';
+	std::cout <<teststring.Append(teststring2);
+	std::cout << '\n';
+	std::cout << '\n';
+	teststring = "abc"; 
+	teststring2 = "efg";
+	std::cout << teststring;
+	std::cout << '\n';
+	std::cout << '\n';
+	std::cout << teststring2;
+	std::cout << '\n';
+	std::cout << '\n';
+	std::cout << (teststring < teststring2);
+	std::this_thread::sleep_for(std::chrono::seconds(10));
 }
